37b24aca795018dcac0880fec8a1a0f9d4c57713
Thomason Zhou <thomason.zhou@uwaterloo.ca>
2024-06-29 00:06:38 +0200

    Resolve import issues with dynamic importing and no ssr

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
-import SkillGraph from "@/components/skillGraph";
+"use client";
+import dynamic from "next/dynamic";
+import React from "react";
+
+const DynamicSkillGraph = dynamic(() => import("../components/skillGraph"), {
+    ssr: false,
+    loading: () => <p>Please wait a few seconds</p>,
+});
 
 export default function Home() {
     return (
         <main>
-            <SkillGraph />
+            <DynamicSkillGraph />
         </main>
     );
 }
--- src/components/skillGraph.tsx
+++ src/components/skillGraph.tsx
@@ -18,4 +18,5 @@ const SkillGraph = ({}) => {
         />
     );
 };
+
 export default SkillGraph;
