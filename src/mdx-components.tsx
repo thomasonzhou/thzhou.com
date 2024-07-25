a082b3e79d33f23d9477d84169fb6272238725d1
Thomason Zhou <thomason.zhou@uwaterloo.ca>
2024-07-25 23:54:31 +0200

    Add styling for simple mdx

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -2,6 +2,11 @@ import type { MDXComponents } from "mdx/types";
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
     return {
+        h1: ({ children }) => (
+            <h1 className="text-2xl mx-4 my-6">{children}</h1>
+        ),
+        h2: ({ children }) => <h2 className="text-xl m-4">{children}</h2>,
+        p: ({ children }) => <p className="m-4">{children}</p>,
         ...components,
     };
 }
